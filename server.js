fd27ccb89902508dfc17ece49eb08908cc7a275b
Dev Jain <devrjain32200@gmail.com>
2019-07-08 21:14:30 -0500

    server is ready to be deployed

--- server.js
+++ server.js
@@ -3,3 +3,22 @@ var path = require("path");
 
 var app = express();
 
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+var apiroutes = require("./app/routing/apiroutes.js");
+var htmlroutes = require("./app/routing/htmlroutes.js");
+
+app.use(apiroutes);
+app.use(htmlroutes);
+
+//listening
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+})
